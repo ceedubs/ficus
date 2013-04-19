e74a4c753b954b2bd92d060e8c41d9013e85255c
Cody Allen <ceedubs@gmail.com>
2013-04-19 08:19:13 -0400

    Improve type safety of the collection value readers

--- src/main/scala/ceedubs/config/readers/CollectionReaders.scala
+++ src/main/scala/ceedubs/config/readers/CollectionReaders.scala
@@ -4,12 +4,21 @@ import com.typesafe.config.Config
 import collection.JavaConverters._
 
 trait CollectionReaders {
-  def listValueReader[A]: ValueReader[List[A]] = new ValueReader[List[A]] {
+  implicit val StringCastable = castableCollectionKind[String]
+  implicit val BooleanCastable = castableCollectionKind[Boolean]
+  implicit val IntCastable = castableCollectionKind[Int]
+  implicit val LongCastable = castableCollectionKind[Long]
+  implicit val DoubleCastable = castableCollectionKind[Double]
+  
+  protected def castableCollectionKind[A]: CastableCollectionKind[A] = new CastableCollectionKind[A] {}
+  
+  implicit def castingListValueReader[A](implicit evidence: CastableCollectionKind[A]): ValueReader[List[A]] = new ValueReader[List[A]] {
     def get(config: Config, path: String): List[A] = config.getList(path).unwrapped().asScala.toList.asInstanceOf[List[A]]
   }
 
-  // weird name so it doesn't look like a setter method
-  def aSetValueReader[A]: ValueReader[Set[A]] = new ValueReader[Set[A]] {
+  implicit def castingSetValueReader[A](implicit evidence: CastableCollectionKind[A]): ValueReader[Set[A]] = new ValueReader[Set[A]] {
     def get(config: Config, path: String): Set[A] = config.getList(path).unwrapped().asScala.toSet.asInstanceOf[Set[A]]
   }
 }
+
+trait CastableCollectionKind[A]
--- src/test/scala/ceedubs/config/readers/CollectionReadersSpec.scala
+++ src/test/scala/ceedubs/config/readers/CollectionReadersSpec.scala
@@ -4,19 +4,67 @@ package readers
 import com.typesafe.config.ConfigFactory
 
 class CollectionReadersSpec extends Spec with CollectionReaders { def is =
-  "The List value reader should" ^
-    "read a list" ! readList ^
+  "The list value reader should" ^
+    "read a list of strings" ! readStringList ^
+    "read a list of booleans" ! readBooleanList ^
+    "read a list of ints" ! readIntList ^
+    "read a list of longs" ! readLongList ^
+    "read a list of doubles" ! readDoubleList ^
                              end ^
-    "The Set value reader should" ^
-    "read a Set" ! readSet
+    "The set value reader should" ^
+    "read a set of strings" ! readStringSet ^
+    "read a set of booleans" ! readBooleanSet ^
+    "read a set of ints" ! readIntSet ^
+    "read a set of longs" ! readLongSet ^
+    "read a set of doubles" ! readDoubleSet
 
-  def readList = {
-    val cfg = ConfigFactory.parseString("myValue = [1, 2, 3]")
-    listValueReader[Int].get(cfg, "myValue") must beEqualTo(List(1, 2, 3))
+  def readStringList = {
+    val cfg = ConfigFactory.parseString("""myValue = ["a", "b", "c"]""")
+    castingListValueReader[String].get(cfg, "myValue") must beEqualTo(List("a", "b", "c"))
   }
 
-  def readSet = {
+  def readBooleanList = {
+    val cfg = ConfigFactory.parseString("myValue = [true, false, true]")
+    castingListValueReader[Boolean].get(cfg, "myValue") must beEqualTo(List(true, false, true))
+  }
+
+  def readIntList = {
     val cfg = ConfigFactory.parseString("myValue = [1, 2, 3]")
-    aSetValueReader[Int].get(cfg, "myValue") must beEqualTo(Set(1, 2, 3))
+    castingListValueReader[Boolean].get(cfg, "myValue") must beEqualTo(List(1, 2, 3))
+  }
+
+  def readLongList = {
+    val cfg = ConfigFactory.parseString("myValue = [2147483648, 2, 3]")
+    castingListValueReader[Long].get(cfg, "myValue") must beEqualTo(List(2147483648L, 2, 3))
+  }
+
+  def readDoubleList = {
+    val cfg = ConfigFactory.parseString("myValue = [0.1, 2.3, 3.4]")
+    castingListValueReader[Long].get(cfg, "myValue") must beEqualTo(List(0.1, 2.3, 3.4))
+  }
+
+  def readStringSet = {
+    val cfg = ConfigFactory.parseString("""myValue = ["a", "b", "c", "c"]""")
+    castingSetValueReader[String].get(cfg, "myValue") must beEqualTo(Set("a", "b", "c"))
+  }
+
+  def readBooleanSet = {
+    val cfg = ConfigFactory.parseString("myValue = [true, false, true]")
+    castingSetValueReader[Boolean].get(cfg, "myValue") must beEqualTo(Set(true, false))
+  }
+
+  def readIntSet = {
+    val cfg = ConfigFactory.parseString("myValue = [1, 2, 3, 3]")
+    castingSetValueReader[Int].get(cfg, "myValue") must beEqualTo(Set(1, 2, 3))
+  }
+
+  def readLongSet = {
+    val cfg = ConfigFactory.parseString("myValue = [2147483648, 2, 3]")
+    castingSetValueReader[Long].get(cfg, "myValue") must beEqualTo(Set(2147483648L, 2, 3))
+  }
+
+  def readDoubleSet = {
+    val cfg = ConfigFactory.parseString("myValue = [0.1, 2.3, 3.4]")
+    castingSetValueReader[Long].get(cfg, "myValue") must beEqualTo(Set(0.1, 2.3, 3.4))
   }
 }
