f84a5fa4d7729f695ed62213650b36aa7fc4b3de
Cody Allen <ceedubs@gmail.com>
2013-04-26 08:38:50 -0400

    Introduce ConfigKey
    A ConfigKey has a config path (String) and a type parameter indicating the type that should be extracted from that path.

--- src/main/scala/net/ceedubs/ficus/FicusConfig.scala
+++ src/main/scala/net/ceedubs/ficus/FicusConfig.scala
@@ -7,6 +7,8 @@ trait FicusConfig {
   def config: Config
 
   def as[A](path: String)(implicit reader: ValueReader[A]): A = reader.read(config, path)
+
+  def apply[A](key: ConfigKey[A])(implicit reader: ValueReader[A]): A = as[A](key.path)
 }
 
 case class SimpleFicusConfig(config: Config) extends FicusConfig
