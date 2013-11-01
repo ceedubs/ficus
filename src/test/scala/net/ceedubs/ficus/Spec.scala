276cebf4a8a94f7066648ffaa9ef1ac4e0cd3bac
Cody Allen <ceedubs@gmail.com>
2013-11-01 10:03:45 -0400

    Use ScalaCheck for StringReader specs

--- src/test/scala/net/ceedubs/ficus/Spec.scala
+++ src/test/scala/net/ceedubs/ficus/Spec.scala
@@ -3,5 +3,12 @@ package net.ceedubs.ficus
 import org.specs2.specification.{FormattingFragments, FragmentsBuilder, BaseSpecification}
 import org.specs2.matcher.MustMatchers
 import org.specs2.ScalaCheck
+import org.scalacheck.Gen
 
-trait Spec extends BaseSpecification with MustMatchers with FragmentsBuilder with FormattingFragments with ScalaCheck
+trait Spec extends BaseSpecification with MustMatchers with FragmentsBuilder with FormattingFragments with ScalaCheck {
+  private[this] val SpecialJsonCharacters = Set('\\', '"')
+
+  private[this] val JsonStringChars = ('\u0020' to '\u007E').filterNot(SpecialJsonCharacters.contains)
+
+  val jsonStringValue = Gen.listOf(JsonStringChars).map(_.mkString)
+}
--- src/test/scala/net/ceedubs/ficus/readers/StringReaderSpec.scala
+++ src/test/scala/net/ceedubs/ficus/readers/StringReaderSpec.scala
@@ -2,13 +2,14 @@ package net.ceedubs.ficus
 package readers
 
 import com.typesafe.config.ConfigFactory
+import org.scalacheck.Prop
 
 class StringReaderSpec extends Spec with StringReader { def is =
   "The String value reader should" ^
     "read a String" ! readString
 
-  def readString = {
-    val cfg = ConfigFactory.parseString("myValue = \"test\"")
-    stringValueReader.read(cfg, "myValue") must beEqualTo("test")
+  def readString = Prop.forAll(jsonStringValue) { string: String =>
+    val cfg = ConfigFactory.parseString("myValue = \"" + string + "\"")
+    stringValueReader.read(cfg, "myValue") must beEqualTo(string)
   }
 }
