b6eabc73374c850391e645c6575786580a6ded9a
Cody Allen <ceedubs@gmail.com>
2013-11-01 22:52:00 -0400

    Use ScalaCheck for Try reader spec

--- src/test/scala/net/ceedubs/ficus/readers/TryReaderSpec.scala
+++ src/test/scala/net/ceedubs/ficus/readers/TryReaderSpec.scala
@@ -2,16 +2,19 @@ package net.ceedubs.ficus.readers
 
 import com.typesafe.config.{ConfigException, Config, ConfigFactory}
 import net.ceedubs.ficus.Spec
+import org.scalacheck.Prop
+import scala.util.Failure
 
 class TryReaderSpec extends Spec with TryReader with AnyValReaders { def is =
   "A try value reader should" ^
     "return a success when a value can be read" ! success ^
     "return a failure when a value cannot be read" ! cannotBeRead ^
-    "handle an unexpected exception type" ! unexpectedException
+    "handle an unexpected exception type" ! unexpectedExceptionType ^
+    "handle an unexpected exception" ! unexpectedException
 
-  def success = {
-    val cfg = ConfigFactory.parseString("myValue = true")
-    tryValueReader[Boolean].read(cfg, "myValue") must beSuccessfulTry[Boolean].withValue(true)
+  def success = check { i: Int =>
+    val cfg = ConfigFactory.parseString(s"myValue = $i")
+    tryValueReader[Int].read(cfg, "myValue") must beSuccessfulTry[Int].withValue(i)
   }
 
   def cannotBeRead = {
@@ -19,11 +22,21 @@ class TryReaderSpec extends Spec with TryReader with AnyValReaders { def is =
     tryValueReader[Boolean].read(cfg, "wrongKey") must beFailedTry[Boolean].withThrowable[ConfigException.Missing]
   }
 
-  def unexpectedException = {
+  def unexpectedExceptionType = {
     val cfg = ConfigFactory.parseString("myValue = true")
     implicit val stringValueReader: ValueReader[String] = new ValueReader[String] {
       def read(config: Config, path: String): String = throw new NullPointerException("oops")
     }
-    tryValueReader[String].read(cfg, "wrongKey") must beFailedTry[String].withThrowable[NullPointerException]("oops")
+    tryValueReader[String].read(cfg, "myValue") must beFailedTry[String].withThrowable[NullPointerException]("oops")
+  }
+
+  def unexpectedException = check { up: Throwable =>
+    val cfg = ConfigFactory.parseString("myValue = true")
+    implicit val stringValueReader: ValueReader[String] = new ValueReader[String] {
+      def read(config: Config, path: String): String = throw up
+    }
+    val expectedMessage = Option(up).map(_.getMessage).orNull
+    tryValueReader[String].read(cfg, "myValue") must beFailedTry[String] and
+      (up.getMessage == expectedMessage must beTrue)
   }
 }
