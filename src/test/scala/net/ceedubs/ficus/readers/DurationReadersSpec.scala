6061f7181f5b2f9d450321ec9a7a011c59bd5ad2
Cody Allen <ceedubs@gmail.com>
2013-11-02 11:07:51 -0400

    Use ScalaCheck for finite duration reader specs

--- src/test/scala/net/ceedubs/ficus/readers/DurationReadersSpec.scala
+++ src/test/scala/net/ceedubs/ficus/readers/DurationReadersSpec.scala
@@ -3,20 +3,21 @@ package readers
 
 import com.typesafe.config.ConfigFactory
 import scala.concurrent.duration._
+import org.scalacheck.{Gen, Prop}
 
 class DurationReadersSpec extends Spec with DurationReaders { def is =
   "The finite duration reader should" ^
     "read a millisecond value" ! readMillis ^
     "read a minute value" ! readMinute
 
-  def readMillis = {
-    val cfg = ConfigFactory.parseString("myValue = 15")
-    finiteDurationReader.read(cfg, "myValue") must beEqualTo(15 millis)
+  def readMillis = prop { i: Int =>
+    val cfg = ConfigFactory.parseString(s"myValue = $i")
+    finiteDurationReader.read(cfg, "myValue") must beEqualTo(i millis)
   }
 
-  def readMinute = {
-    val cfg = ConfigFactory.parseString("myValue = \"15 minutes\"")
-    finiteDurationReader.read(cfg, "myValue") must beEqualTo(15 minutes)
+  def readMinute = Prop.forAll(Gen.choose(1.5e-8.toInt, 1.5e8.toInt)) { i: Int =>
+    val cfg = ConfigFactory.parseString("myValue = \"" + i + " minutes\"")
+    finiteDurationReader.read(cfg, "myValue") must beEqualTo(i minutes)
   }
 
 }
