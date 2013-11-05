bdac2cc4214454463a989c3ff1feeee76af0cd53
Cody Allen <ceedubs@gmail.com>
2013-11-04 22:47:17 -0500

    Add ValueReader for FicusConfig

--- src/main/scala/net/ceedubs/ficus/readers/ConfigReader.scala
+++ src/main/scala/net/ceedubs/ficus/readers/ConfigReader.scala
@@ -1,11 +1,14 @@
 package net.ceedubs.ficus.readers
 
 import com.typesafe.config.Config
+import net.ceedubs.ficus.{SimpleFicusConfig, FicusConfig}
 
 trait ConfigReader {
   implicit val configValueReader: ValueReader[Config] = new ValueReader[Config] {
     def read(config: Config, path: String): Config = config.getConfig(path)
   }
+
+  implicit val ficusConfigValueReader: ValueReader[FicusConfig] = configValueReader.map(SimpleFicusConfig)
 }
 
 object ConfigReader extends ConfigReader
--- src/test/scala/net/ceedubs/ficus/readers/ConfigReaderSpec.scala
+++ src/test/scala/net/ceedubs/ficus/readers/ConfigReaderSpec.scala
@@ -1,11 +1,15 @@
 package net.ceedubs.ficus
 package readers
 
-import com.typesafe.config.ConfigFactory
+import com.typesafe.config.{Config, ConfigFactory}
+import FicusConfig._
 
-class ConfigReaderSpec extends Spec with ConfigReader with AnyValReaders { def is = s2"""
+class ConfigReaderSpec extends Spec { def is = s2"""
   The Config value reader should
     read a config $readConfig
+    implicitly read a config $implicitlyReadConfig
+    read a ficus config $readFicusConfig
+    implicitly read a ficus config $implicitlyReadFicusConfig
   """
 
   def readConfig = prop { i: Int =>
@@ -16,5 +20,35 @@ class ConfigReaderSpec extends Spec with ConfigReader with AnyValReaders { def i
         |}
       """.stripMargin)
     configValueReader.read(cfg, "myConfig").getInt("myValue") must beEqualTo(i)
+  }
+
+  def implicitlyReadConfig = prop { i: Int =>
+    val cfg = ConfigFactory.parseString(
+      s"""
+        |myConfig {
+        |  myValue = $i
+        |}
+      """.stripMargin)
+    cfg.as[Config]("myConfig").getInt("myValue") must beEqualTo(i)
+  }
+
+  def readFicusConfig = prop { i: Int =>
+    val cfg = ConfigFactory.parseString(
+      s"""
+        |myConfig {
+        |  myValue = $i
+        |}
+      """.stripMargin)
+    ficusConfigValueReader.read(cfg, "myConfig").as[Int]("myValue") must beEqualTo(i)
+  }
+
+  def implicitlyReadFicusConfig = prop { i: Int =>
+    val cfg = ConfigFactory.parseString(
+      s"""
+        |myConfig {
+        |  myValue = $i
+        |}
+      """.stripMargin)
+    cfg.as[FicusConfig]("myConfig").as[Int]("myValue") must beEqualTo(i)
   }
 }
