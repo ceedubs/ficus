cc3250df308ef0a8d13cf6c88cd0ad12a0d4d198
Ian Tabolt <ian.tabolt@mlb.com>
2016-11-15 16:23:00 -0500

    Read finite duration from nanos to keep unit

--- src/test/scala/net/ceedubs/ficus/readers/DurationReadersSpec.scala
+++ src/test/scala/net/ceedubs/ficus/readers/DurationReadersSpec.scala
@@ -2,6 +2,7 @@ package net.ceedubs.ficus
 package readers
 
 import com.typesafe.config.ConfigFactory
+
 import scala.concurrent.duration._
 import org.scalacheck.{Gen, Prop}
 
@@ -9,6 +10,7 @@ class DurationReadersSpec extends Spec with DurationReaders { def is = s2"""
   The finite duration reader should
     read a millisecond value $readMillis
     read a minute value $readMinutes
+    read a days value into days $readDaysUnit
   """
 
   def readMillis = prop { i: Int =>
@@ -16,9 +18,14 @@ class DurationReadersSpec extends Spec with DurationReaders { def is = s2"""
     finiteDurationReader.read(cfg, "myValue") must beEqualTo(i millis)
   }
 
-  def readMinutes = Prop.forAll(Gen.choose(1.5e-8.toInt, 1.5e8.toInt)) { i: Int =>
+  def readMinutes = Prop.forAll(Gen.choose(-1.5e8.toInt, 1.5e8.toInt)) { i: Int =>
     val cfg = ConfigFactory.parseString("myValue = \"" + i + " minutes\"")
     finiteDurationReader.read(cfg, "myValue") must beEqualTo(i minutes)
   }
 
+  def readDaysUnit = Prop.forAll(Gen.choose(-106580, 106580)) { i: Int =>
+    val str = i + " day" + (if (i == 1) "" else "s")
+    val cfg = ConfigFactory.parseString(s"""myValue = "$str" """)
+    finiteDurationReader.read(cfg, "myValue").toString must beEqualTo(str)
+  }
 }
