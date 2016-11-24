ae15aeb424ecd920c8d3afc746f8963e8674bc1f
eyal farago <eyal.farago@gmail.com>
2016-11-25 00:36:27 +0200

    eitherReader: valueReader implementation for Either[A,B] given A and B have their own value readers.

---
+++ src/main/scala/net/ceedubs/ficus/readers/EitherReader.scala
@@ -0,0 +1,22 @@
+package net.ceedubs.ficus.readers
+import com.typesafe.config.{Config, ConfigException}
+
+/**
+  * Created by eyalf on 11/24/2016.
+  */
+trait EitherReader {
+  implicit def eitherReader[L,R]( implicit lReader : ValueReader[L], rReader : ValueReader[R]) : ValueReader[Either[L,R]] =
+    new ValueReader[Either[L,R]]{
+      /** Reads the value at the path `path` in the Config */
+      override def read(config: Config, path: String): Either[L, R] = {
+        TryReader.tryValueReader(rReader).read( config, path )
+        .map( Right(_) )
+        .recover{
+          case _ : ConfigException => Left( lReader.read(config, path))
+        }
+        .get
+      }
+    }
+}
+
+object EitherReader extends EitherReader
